4511342957e6e1a7605bbbbf0afe1c9f915537c0
Mgphone <51868037+Mgphone@users.noreply.github.com>
2023-10-27 23:52:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+} from "@react-google-maps/api";
+const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [userLocation, setUserLocation] = useState("");
+  const [userDestination, setUserDestination] = useState("");
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+    libraries: ["places"],
+  });
+  // if (loadError) {
+  //   return <div>That is an Error to load</div>;
+  // }
+  return isLoaded ? (
+    <div className="mapcontainer">
+      <GoogleMap
+        center={{ lat: 51.588745961637194, lng: -0.2205496225132663 }}
+        mapContainerStyle={{ width: "100%", height: "100%" }}
+        zoom={15}
+      ></GoogleMap>
+      <div className="quotesearch">
+        <form className="formsearch">
+          <label htmlFor="search1" />
+          <Autocomplete>
+            <input type="text" placeholder="enter your location" />
+          </Autocomplete>
+          <label htmlFor="search2" />
+          <Autocomplete>
+            <input type="text" placeholder="enter your destination" />
+          </Autocomplete>
+          <button>Quote</button>
+        </form>
+      </div>
     </div>
+  ) : (
+    <></>
   );
 }
 
