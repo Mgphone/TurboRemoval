3c2bb4f96fa77a911c6ccafef1c1fbdab73e1393
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-01-07 12:24:51 +0000

    add cors origin

--- manAndVanBackend/config/whiteList.js
+++ manAndVanBackend/config/whiteList.js
@@ -1,3 +1,7 @@
 // whiteList.js
-const whiteList = ["http://localhost:3000", "http://192.168.1.216:3000"];
+const whiteList = [
+  "http://localhost:3000",
+  "http://192.168.1.216:3000",
+  "https://lifitinglondonremoval.netlify.app",
+];
 module.exports = whiteList;
