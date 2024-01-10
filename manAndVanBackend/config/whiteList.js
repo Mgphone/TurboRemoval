7ae2e224101142cfd7a142654ad82ccd9769c2e4
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-01-10 03:30:57 +0000

    initial commit for LiftingLondon

--- manAndVanBackend/config/whiteList.js
+++ manAndVanBackend/config/whiteList.js
@@ -1,7 +1,9 @@
 // whiteList.js
 const whiteList = [
-  "http://localhost:3000",
-  "http://192.168.1.216:3000",
-  `${process.env.MY_URL_FRONT}`,
+  // "http://localhost:3000",
+  // "http://192.168.1.216:3000",
+  // `${process.env.MY_URL_FRONT}`,
+  // process.env.MY_URL_FRONT,
+  process.env.MY_URL_FRONT,
 ];
 module.exports = whiteList;
